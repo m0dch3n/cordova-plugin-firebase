2ee452237be34af5710a2dfed3a47f23be8cce5e
m0dch3n <ben@thinnes.lu>
2019-05-09 19:23:59 +0200

    1.0.0 cordova-plugin-firebase-m0dch3n

--- bump-version.js
+++ bump-version.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const version = require('./package.json').version;
 
 let data = fs.readFileSync('./plugin.xml', "utf8");
-data = data.replace(/plugin id="cordova-plugin-firebase" version="[^"]+"/, `plugin id="cordova-plugin-firebase" version="${version}"`);
+data = data.replace(/plugin id="cordova-plugin-firebase-m0dch3n" version="[^"]+"/, `plugin id="cordova-plugin-firebase-m0dch3n" version="${version}"`);
 fs.writeFileSync('./plugin.xml', data);
